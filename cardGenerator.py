c6d3eae1ed0ef4b1c7e6b18066ef54497dde07f8
Madeline Mayans <82738387+mmayans@users.noreply.github.com>
2023-11-08 17:06:30 -0700

    edited formatting of cards
    added new method to the hello world file to change the shape/size of the image, also added 3 cards following design pattern

--- cardGenerator.py
+++ cardGenerator.py
@@ -8,13 +8,13 @@ import copy
 def create_cards() :
     list1 = [" red: ", " blue: ", " green: ", " yellow: " ]
     font = "Arial"
-    size = 32
+    size = 48
     realcolor = (0,0,0)
-    for color in list1:
+    '''for color in list1:
         string1 = color
         for i in range(3):
             pdb.python_fu_hello_world(string1 + str(i + 1), font, size, realcolor)
-            '''list2 = copy.deepcopy(list1)
+            list2 = copy.deepcopy(list1)
             try:
                 list2.remove(color)
             except Exception as e:
@@ -25,7 +25,7 @@ def create_cards() :
                         pdb.python_fu_hello_world(string1 + str(i + 1) + " " + color2 + str(j + 1), font, size, realcolor)
                     
         except Exception as e:
-            list2 = list2'''
+            list2 = list2
     
     for color in list1:
         string1 = color
@@ -34,7 +34,29 @@ def create_cards() :
                 string2 = color2
                 if string1 != string2:
                     for j in range(3):
-                        pdb.python_fu_hello_world(string1 + str(i + 1) + " " + color2 + str(j + 1), font, size, realcolor)
+                        ##added new line character for spacing reasons
+                        pdb.python_fu_hello_world(string1 + str(i + 1) + "\n" + color2 + str(j + 1), font, size, realcolor)'''
+
+    ##I simplified everything into one horrible loop hope this helps
+    for color in list1:
+        string1 = color
+        for i in range(3):
+            ##all of the single cards generated here
+            pdb.python_fu_hello_world(string1 + str(i + 1), font, size, realcolor)
+            for color2 in list1:
+                string2 = color2
+                if string1 != string2:
+                    for j in range(i + 1):
+                        ##all of the 2 cards here (1,1) (2,1) (2,2) (3,1),(3,2)(3,3)
+                        pdb.python_fu_hello_world(string1 + str(i + 1) + "\n" + color2 + str(j + 1), font, size, realcolor)
+                        for color3 in list1:
+                            string3 = color3
+                            if string3!=string2 and string3 != string1:
+                             ##all of the 3 cards here (1,1,1) (2,1,1) (2,2,2) (3,1,1),(3,2,2)(3,3,3)
+                             ##follows design pattern from meeting without generating every possible combination (so no 1,2,3 or ,2,1,2)
+                             ##there are a lot of duplicates but we knew that would be the case
+                             pdb.python_fu_hello_world(string1 + str(i + 1) + "\n" + color2 + str(j + 1)+ "\n" + color3 + str(j + 1), font, size, realcolor)
+                        
 
 
 
@@ -42,7 +64,7 @@ register(
     "python_fu_create_cards",
     "Create cards for the IE game",
     "Create cards",
-    "David Williams and Maddie",
+    "David Williams and Maddie Mayans",
     "2023",
     "2023",
     "Create Cards (py)",
--- helloWorldPython.py
+++ helloWorldPython.py
@@ -4,13 +4,41 @@
 
 from gimpfu import *
 
+def create_rectangle(image, drawable, x, y, width, height):
+    ## Set the foreground color to black
+    pdb.gimp_context_set_foreground((0, 0, 0, 255))
+
+    ## Create a new layer for the rectangle
+    layer = pdb.gimp_layer_new(image, width, height, RGBA_IMAGE, "Rectangle", 100, NORMAL_MODE)
+    pdb.gimp_image_insert_layer(image, layer, None, 0)
+
+    ##set the position of the layer
+    layer.set_offsets(20, 20)
+
+    ##Create a selection in the shape of the rectangle
+    pdb.gimp_rect_select(image, x, y, width, height, CHANNEL_OP_REPLACE, False, 0)
+
+    ##Edit the selection to add a border
+    pdb.gimp_selection_border(image, 3)
+
+    ## Fill the selection with the foreground color to create the border
+    pdb.gimp_edit_fill(layer, FOREGROUND_FILL)
+
+    ## Clear the selection
+    pdb.gimp_selection_none(image)
+
+    ## Refresh the display
+    gimp.displays_flush()
+
 def hello_world(initstr, font, size, color) :
-    # First do a quick sanity check on the font
-    if font == 'Comic Sans MS' :
-        initstr = "Comic Sans? Are you sure?"
+    ## Maddie Notes:
+    ## I removed the comic sans check 
+    ##also changed the image size
+    ##it's no longer the size of the text
+    ##it is now 250 x 350, (a standard playing card is 2.5 x 3.5 inches)
 
     # Make a new image. Size 10x10 for now -- we'll resize later.
-    img = gimp.Image(1, 1, RGB)
+    img = gimp.Image(250, 350, RGB)
 
     # Save the current foreground color:
     pdb.gimp_context_push()
@@ -19,18 +47,22 @@ def hello_world(initstr, font, size, color) :
     gimp.set_foreground(color)
 
     # Create a new text layer (-1 for the layer means create a new layer)
-    layer = pdb.gimp_text_fontname(img, None, 0, 0, initstr, 10,
+    layer = pdb.gimp_text_fontname(img, None, 10, 25, initstr, 10,
                                    True, size, PIXELS, font)
 
     # Resize the image to the size of the layer
-    img.resize(layer.width, layer.height, 0, 0)
+    ##I commented this out because it changes the size of the image
+   # img.resize(layer.width, layer.height, 0, 0)
 
     # Background layer.
     # Can't add this first because we don't know the size of the text layer.
-    background = gimp.Layer(img, "Background", layer.width, layer.height,
+    ##yes we can because we do know that information bc its hardcoded now
+    background = gimp.Layer(img, "Background", 250, 350,
                             RGB_IMAGE, 100, NORMAL_MODE)
     background.fill(BACKGROUND_FILL)
     img.add_layer(background, 1)
+
+    create_rectangle(img, layer, 20, 20, 210, 310)
 
     # Create a new image window
     gimp.Display(img)
@@ -44,7 +76,7 @@ register(
     "python_fu_hello_world",
     "Hello world image",
     "Create a new image with your text string",
-    "Akkana Peck",
+    "Akkana Peck (edits by Maddie Mayans)",
     "Akkana Peck",
     "2010",
     "Hello world (Py)...",
